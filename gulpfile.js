7c95679d7f0dceb668541c6ff60754637780ec64
Benjamin Turner <benjamin@passionsplay.com>
2016-10-24 18:26:57 -0700

    Add browser-sync to gulpfile to automatically compile svgs on save

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,24 @@
 var gulp = require("gulp");
+var browserSync = require("browser-sync")
 var svgstore = require("gulp-svgstore");
 var svgmin = require("gulp-svgmin");
 var path = require("path");
+
+// Serve site using browser-sync.
+gulp.task("serve", function(){
+	browserSync.init({
+		server: "public",
+		files: "public"
+	});
+	gulp.watch("./icons/**/*.svg", ['svg-watch']);
+	gulp.watch("public").on('change', browserSync.reload);
+});
+
+// Trigger browsersync reload after the svgs have been compiled.
+gulp.task("svg-watch", ['svgstore'], function(done) {
+	browserSync.reload();
+	done();
+});
 
 gulp.task("svgstore", function(){
 	return gulp
